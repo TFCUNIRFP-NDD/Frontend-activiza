825a4a57514179e1387b69745c28e743c4d9df3a
nestorsan77 <nestorsan77@gmail.com>
2024-05-12 11:57:22 +0200

    Diseño de Añadir ejercicio incorporado

---
+++ app/src/main/java/com/activiza/activiza/ui/view/fragmentos/AnadirEjerciciosFragment.kt
@@ -0,0 +1,33 @@
+package com.activiza.activiza.ui.view.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.activiza.activiza.R
+import com.activiza.activiza.databinding.FragmentAnadirEjerciciosBinding
+import com.activiza.activiza.databinding.FragmentAnadirRutinaBinding
+
+class AnadirEjerciciosFragment : Fragment() {
+
+    private var _binding: FragmentAnadirEjerciciosBinding? = null
+    private val binding get() = _binding!!
+    val args:AnadirEjerciciosFragmentArgs by navArgs()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentAnadirEjerciciosBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        initUI()
+        return rootView
+    }
+
+    private fun initUI() {
+
+    }
+
+}
