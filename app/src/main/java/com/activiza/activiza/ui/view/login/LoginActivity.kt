4854c47ddacc6600533cf3f9970fbdc42d27f861
david <davidd.fdez@gmail.com>
2024-05-14 11:20:53 +0200

    settings y diseño login

--- app/src/main/java/com/activiza/activiza/ui/view/login/LoginActivity.kt
+++ app/src/main/java/com/activiza/activiza/ui/view/login/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import com.activiza.activiza.data.AuthData
@@ -36,7 +37,16 @@ class LoginActivity : AppCompatActivity() {
 
         sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE)
 
-        db = ActivizaDataBaseHelper(binding.tvRegister.context)
+        db = ActivizaDataBaseHelper(binding.layoutRegister.context)
+
+        //Cambio de vista de layout LOGIN/REGISTER
+        binding.tvSignIn.setOnClickListener {
+            showLoginLayout()
+        }
+
+        binding.tvSignUp.setOnClickListener {
+            showRegisterLayout()
+        }
 
         // Configuración de Retrofit
         val retrofit = Retrofit.Builder()
@@ -66,11 +76,77 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        // Funcionalidad al hacer clic en el enlace para registrarse
-        binding.tvRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+        // Funcionalidad del registro
+        //Funcionalidad al clickar en las cajas de email, password y nombre
+        binding.etEmailSignup.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                validateEmailFormat()
+            }
         }
+
+        binding.etNameSignup.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                usernameEmpty()
+            }
+        }
+
+
+        binding.etPasswordSignup.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                binding.tiPasswordSignup.setHelperTextEnabled(false)
+
+                validatePasswordFormat()
+            }else{
+                binding.tiPasswordSignup.setHelperTextEnabled(true)
+                binding.tiPasswordSignup.helperText = "La contraseña debe tener al menos: " +
+                        "8 caracteres de longitud, una letra mayúscula,una letra minúscula,un número y un carácter especial como @, #,$,/ etc."
+            }
+        }
+
+        //Funcionalidad boton Register
+        binding.btnRegister.setOnClickListener {
+            val nameNotEmpty = binding.etNameSignup.text.toString().isNotEmpty()
+            val emailNotEmpty = binding.etEmailSignup.text.toString().isNotEmpty()
+            val passwordNotEmpty = binding.etPasswordSignup.text.toString().isNotEmpty()
+            val validEmailFormat =
+                Patterns.EMAIL_ADDRESS.matcher(binding.etEmailSignup.text.toString()).matches()
+            val validPasswordFormat = "^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=,!.?]).*$".toRegex()
+
+
+
+            when {
+                nameNotEmpty && emailNotEmpty && passwordNotEmpty -> {
+                    if (validEmailFormat && validPasswordFormat.matches(binding.etPasswordSignup.text.toString())) {
+                        val intent = Intent(this, OnboardingActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        if (!validEmailFormat) {
+                            Toast.makeText(
+                                this,
+                                "Por favor, introduce un email válido",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        if (!validPasswordFormat.matches(binding.etPasswordSignup.text.toString())) {
+                            Toast.makeText(
+                                this,
+                                "Por favor, introduce una contraseña válida",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+
+                else -> {
+                    Toast.makeText(this, "Por favor, rellene todos los campos", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+
+
     }
 
     fun authenticateUser(authData: AuthData) {
@@ -120,5 +196,62 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
-    // Aquí puedes agregar más métodos según sea necesario
+    private fun showLoginLayout(){
+        binding.layoutLogin.visibility = View.VISIBLE
+        binding.layoutRegister.visibility = View.GONE
+
+    }
+
+    private fun showRegisterLayout() {
+
+        binding.layoutLogin.visibility = View.GONE
+        binding.layoutRegister.visibility = View.VISIBLE
+    }
+
+    //Métodos de validacion de registro
+    //Valida si el formato de email es correcto
+    private fun validateEmailFormat(): Boolean {
+        val validEmailFormat =
+            Patterns.EMAIL_ADDRESS.matcher(binding.etEmailSignup.text.toString()).matches()
+        if (!validEmailFormat) {
+            binding.tiEmailSignup.error = "Por favor, introduce un email válido"
+        } else {
+            binding.tiEmailSignup.error = null
+        }
+        return validEmailFormat
+    }
+
+
+    //Valida si el formato de la password es correcto
+    private fun validatePasswordFormat(): Boolean {
+        val validPasswordFormat = "^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=,!]).*$".toRegex()
+        val validPassword = validPasswordFormat.matches(binding.etPasswordSignup.text.toString())
+        //val validPassword = validPasswordFormat.find(binding.etPasswordSignup.text.toString()) != null
+
+        if (!validPassword) {
+            binding.tiPasswordSignup.error = "Formato no válido"
+            binding.tiPasswordSignup.setHelperTextEnabled(false)
+
+        } else {
+            binding.tiPasswordSignup.error = null
+
+
+        }
+        return validPassword
+    }
+
+    //Comprueba si el campo de nombre está vacío
+    private fun usernameEmpty(): Boolean {
+        var usernameEmpty = true
+        if (binding.etNameSignup.text.toString().isNotEmpty()){
+            binding.tiName.error = null
+            usernameEmpty = false
+        } else {
+            binding.tiName.error = "Este campo no puede estar vacío"
+            usernameEmpty = true
+        }
+        return usernameEmpty
+    }
+
+
 }
