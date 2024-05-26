964afdad41df54011cdea442d995dfea26db99d2
nestorsan77 <nestorsan77@gmail.com>
2024-05-26 23:11:51 +0200

    Asignadas las animaciones del navGraph por defecto a falta de los intents y los menus principales

--- app/src/main/java/com/activiza/activiza/ui/view/fragmentos/EntrenamientosFragment.kt
+++ app/src/main/java/com/activiza/activiza/ui/view/fragmentos/EntrenamientosFragment.kt
@@ -1,11 +1,13 @@
 package com.activiza.activiza.ui.view.fragmentos
 
-import android.content.Context
+
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -16,7 +18,6 @@ import com.activiza.activiza.databinding.FragmentEntrenamientosBinding
 import com.activiza.activiza.domain.APIListener
 import com.activiza.activiza.domain.ActivizaDataBaseHelper
 import com.activiza.activiza.ui.viewmodel.RutinasAdapter
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -29,11 +30,11 @@ class EntrenamientosFragment : Fragment() {
     private var _binding: FragmentEntrenamientosBinding? = null
     private val binding get() = _binding!!
 
-    //cambiar la posicion de > < que hay pero hacia arriba y abajo por defecto esta abajo
-    private var isArrowUp = false // Variable de estado
-    private var genero:String = ""
-    lateinit var db: ActivizaDataBaseHelper
-    var rutina: RutinaData? = null
+    // Variable de estado para manejar la flecha y la visibilidad de los ítems
+    private var isArrowUp = false
+    private lateinit var db: ActivizaDataBaseHelper
+    private var rutina: RutinaData? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -53,7 +54,7 @@ class EntrenamientosFragment : Fragment() {
     private fun mostrarPanelDeControl() {
         db = ActivizaDataBaseHelper(binding.tvRutinaName.context)
         rutina = db.obtenerPrimeraRutina()
-        if(rutina != null){
+        if (rutina != null) {
             binding.hsvSubMenu.visibility = View.VISIBLE
             binding.btnPanelDeControl.setOnClickListener {
                 findNavController().navigate(R.id.action_entrenamientosFragment_to_panelDeControlFragment)
@@ -69,7 +70,6 @@ class EntrenamientosFragment : Fragment() {
         val detallesUsuario = db.getDetallesUsuario()
         binding.tvRutinaName.text = "Rutinas ${detallesUsuario!!.genero}".uppercase()
         anadirDatosRetrofit()
-
     }
 
     private fun anadirDatosRetrofit() {
@@ -95,9 +95,11 @@ class EntrenamientosFragment : Fragment() {
                 if (rutinas != null) {
                     // Configurar el RecyclerView en el hilo principal
                     binding.rvRutinas.adapter = RutinasAdapter(rutinas) { rutinaId ->
-                        findNavController().navigate(EntrenamientosFragmentDirections.actionEntrenamientosFragmentToRutinaIDFragment(
-                            id = rutinaId
-                        ))
+                        findNavController().navigate(
+                            EntrenamientosFragmentDirections.actionEntrenamientosFragmentToRutinaIDFragment(
+                                id = rutinaId
+                            )
+                        )
                     }
                     binding.rvRutinas.layoutManager = LinearLayoutManager(requireContext())
                 } else {
@@ -115,21 +117,37 @@ class EntrenamientosFragment : Fragment() {
         }
     }
 
-
     private fun inicializarEventos() {
         binding.ivArrow.setOnClickListener {
             // Cambiar la imagen y el estado de acuerdo con el estado actual
             if (isArrowUp) {
                 binding.ivArrow.setImageResource(R.drawable.ic_arrow_down)
+                // Aplicar la animación de entrada (desplazamiento hacia abajo)
+                val slideIn = AnimationUtils.loadAnimation(context, R.anim.slide_down)
+                binding.rvRutinas.startAnimation(slideIn)
+                binding.rvRutinas.visibility = View.VISIBLE
             } else {
                 binding.ivArrow.setImageResource(R.drawable.ic_arrow_up)
+                // Aplicar la animación de salida (desplazamiento hacia arriba)
+                val slideOut = AnimationUtils.loadAnimation(context, R.anim.slide_up)
+                slideOut.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation) {}
+
+                    override fun onAnimationEnd(animation: Animation) {
+                        // Después de que la animación termine, ocultar el RecyclerView
+                        binding.rvRutinas.visibility = View.GONE
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation) {}
+                })
+                binding.rvRutinas.startAnimation(slideOut)
             }
             // Cambiar el estado
             isArrowUp = !isArrowUp
         }
     }
 
-    private fun getRetrofit(): Retrofit{
+    private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("http://34.163.215.184/activiza/api/")
             .addConverterFactory(GsonConverterFactory.create())
